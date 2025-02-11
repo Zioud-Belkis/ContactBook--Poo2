8588e9769ac1b34641b5615bc0cf8829fc7d9e91
AdnaneTaleb10 <adnane102464@gmail.com>
2025-02-11 21:31:26 +0100

    change the visibility of the attribute in Contact + remove getters and setters

--- Contact.java
+++ Contact.java
@@ -2,46 +2,16 @@ package poo2;
 
 public abstract class Contact {
 
-	    public String name;
-	    public String phoneNumber;
-	    public String email;
+	    protected  String name;
+	    protected  String phoneNumber;
+	    protected  String email;
 
 	    public Contact(String name, String phoneNumber, String email) {
 	        this.name = name;
 	        this.phoneNumber = phoneNumber;
 	        this.email = email;
 	    }
-
-	    public String getName() {
-			return name;
-		}
-
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-
-		public String getPhoneNumber() {
-			return phoneNumber;
-		}
-
-
-		public void setPhoneNumber(String phoneNumber) {
-			this.phoneNumber = phoneNumber;
-		}
-
-
-		public String getEmail() {
-			return email;
-		}
-
-
-		public void setEmail(String email) {
-			this.email = email;
-		}
-
-
+		
 		public void displayDetails() {
            System.out.println(this.toString());
 	    }
@@ -49,6 +19,6 @@ public abstract class Contact {
 	    public String toString() {
 	    	   return "Name :" + name + "\n Number phone :" + phoneNumber + "\nEmail :" + email;
 	       }
+
 	    public abstract Contact copy();
-	    
        }
